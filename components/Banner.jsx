a93a7c7652d0b750bf8bb2c2db1a641280f3877d
Muhammad Daud <86343612+MDaud32@users.noreply.github.com>
2022-05-12 17:00:01 +0500

    setting up banner component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,14 +1,29 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Banner = ({ title, subTitle, imgUrl }) => {
   return (
-    <div className="flex relative w-full h-80">
+    <div className="flex relative w-full h-[80vh]">
       <div className="absolute z-10">
-        <div className="text-white">
-          <h2>{title}</h2>
-          <h3>{subTitle}</h3>
-          <div>
-            <button>Play</button>
+        <div className="text-white px-[4rem] py-[4rem] text-center">
+          <div className="flex flex-row gap-2">
+            <p className="text-red-600 text-5xl font-bold">N</p>
+            <p className="mt-3">S E R I E S</p>
+          </div>
+          <h2 className="text-3xl uppercase font-semibold stroke-[4px] mt-2 stroke-black">
+            {title}
+          </h2>
+          <h3 className="text-2xl uppercase text-slate-400">{subTitle}</h3>
+          <div className="flex justify-center">
+            <button className="px-8 py-2 rounded-md mt-5 bg-white text-black flex items-center">
+              <Image
+                src="/static/play-icon.svg"
+                alt="this is auther pic"
+                width={40}
+                height={40}
+              />
+              <span>Play</span>
+            </button>
           </div>
         </div>
       </div>
